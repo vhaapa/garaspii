aff7dad8141023fa1c031c51533b9434a16539d9
eVill <aspenfield@outlook.com>
2018-12-14 00:34:58 +0200

    new sensors and camera tweets

---
+++ create_service.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#run .py as a service
+
+#create service file
+#cd /lib/systemd/system/
+echo "[Unit]" > garaspii.service
+echo "Description=eVill's Garbage" >> garaspii.service
+echo "After=multi-user.target" >> garaspii.service
+echo "[Service]" >> garaspii.service
+echo "Type=simple" >> garaspii.service
+echo "ExecStart=/usr/bin/python /home/pi/garaspii/main.py" >> garaspii.service
+echo "Restart=on-abort" >> garaspii.service
+echo "[Install]" >> garaspii.service
+echo "WantedBy=multi-user.target" >> garaspii.service
+
+#rights to .service file
+sudo chmod 644 garaspii.service
+
+#rights to .py file
+#cd /home/pi/garaspii/
+#chmod +x main.py
+
+#start service and make it persistent
+#sudo systemctl daemon-reload
+#sudo systemctl enable garaspii.service
+#sudo systemctl start garaspii.service
+
+#stop service, disable service
+# sudo systemctl stop garaspii.service
+# sudo systemctl disable garaspii.service
+
+#check log
+# sudo journalctl -f -u garaspii.service
--- garaspii.py
+++ garaspii.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
-#python version 2
 
 import sys
 import time
-import grovepi
-import threading
+import grovepi          #grovepi.py file, code for sensors and arduino board
+#import threading
 import twitter
-import keys 		#own file keys.py with keys to different APIs
+import keys 		#keys.py with keys to different APIs
 from picamera import PiCamera
 
 
@@ -27,16 +26,16 @@ def pushButton(pin):		#if button pushed, return True
 	else:
 		return False
 
-def resetAll(led):		#reset all output actors e.g. leds 
-	grovepi.digitalWrite(led,0)
+def resetActor(actor):		#reset all output actors e.g. leds 
+	grovepi.digitalWrite(actor,0)
 	return
 
-def repeatTime():		#timed actions and repeats, ATM prints repeat every 5 secs
-	if not pushButton(button): 
-		threading.Timer(5.0, repeatTime).start()
-		print ("Repeat")
-	else:
-		return
+#def repeatTime():		#timed actions and repeats, ATM prints repeat every 5 secs
+#	if not pushButton(button): 
+#		threading.Timer(5.0, repeatTime).start()
+#		print ("Repeat")
+#	else:
+#		return
 
 def connectTwitter():		#connect to twitter and print it
 	api = twitter.Api(consumer_key=keys.twitterCK(),consumer_secret=keys.twitterCS(),access_token_key=keys.twitterATK(),access_token_secret=keys.twitterATS())
@@ -47,12 +46,10 @@ def measureDistance(ultrasonic):		#ultrasonic measurement
 	distance = grovepi.ultrasonicRead(ultrasonic)
 	return distance
 
-def setupLeds(blue, red):
-	grovepi.pinMode(blue,"OUTPUT")
-	grovepi.pinMode(red,"OUTPUT")
-	resetAll(blue)
-	resetAll(red)
-	return "Led setup complete."
+def setupLed(led):
+	grovepi.pinMode(led,"OUTPUT")
+	resetActor(led)
+	return
 
 def takePhoto():
 	filepath = "/home/pi/pics/" + time.strftime("%Y%m%d-%H%M%S") + ".jpg" 
@@ -64,27 +61,36 @@ def takePhoto():
 	cam.close()
 	return filepath
 
-#pins, sensors and actors
-blue = 5
-red = 6
+# setting up...
+# pins, sensors and actors
+led = {"red":6, "blue":5, "green":3}
+#blue = 5
+#red = 6
 #green = x 
 button = 2
 temphum = 4
 ultrasonic = 8
 
-#variables
+# variables
 temp = 0
 hum = 0
 api = connectTwitter()   #create a twitter connection
 dist= 0
+#message = ["Status:", "Door open?", "ALERT!", "Ok?"] 
 msg = "Status:"
 start = time.time() 	#used as timer
 door = True		#door closed = true, open = false
 doorLogic = True	#variable used with  door status changes
+photoTaken = False	#used for photo logic
+photoTime = time.time() #last picture taken, used as timer
 
 #main program
 
-print setupLeds(blue,red)
+#print setupLeds(led["blue"],led["red"])
+#setup leds
+for k in led:
+	setupLed(led[k])
+	print k + " led setup done."
 
 #repeatTime()	#for testing timed repeted actions
 
@@ -95,8 +101,10 @@ while True:
 #		ledBlink(red,2)		#testing that loop is working
 
 		if pushButton(button):		#break loop and end the program
-			resetAll(red)
+			for k in led:
+				resetActor(led[k])
 			print ("Button pushed, The End!")
+			api.PostUpdate("Button pushed, The End. Bye!")
 			break
 
 #measure distance and print it, use red led
@@ -106,18 +114,35 @@ while True:
 			dist = measureDistance(ultrasonic)
 
 #		print dist, "cm"
-		if dist < 100:
-			grovepi.digitalWrite(red,31)
-		else:
-			resetAll(red)
-
-#door open or closed, tweet if changed
-		if dist > 100:
+		if dist < 120:
 			door = False
-			msg = "Door open!"
-		else:
+			grovepi.digitalWrite(led["red"],31)
+			msg = "MOTION"
+			if photoTaken == False and time.time() - photoTime > 300:			
+				photo = takePhoto()
+				photoTime = time.time()
+                               	print "ZAP: " + photo 
+			#	print "PHOTO"
+				photoTaken = True
+				api.PostUpdate("Meedio", media=photo)
+		elif dist >= 120 and dist < 130:
 			door = True
 			msg = "OK?"
+			resetActor(led["red"])
+			photoTaken = False
+		else:
+			door =False
+			msg = "Door open!"
+			resetActor(led["red"])
+			photoTaken = False
+
+#door open or closed, tweet if changed
+#		if dist > 120:
+#			door = False
+#			msg = "Door open!"
+#		else:
+#			door = True
+#			msg = "OK?"
 
 		if door != doorLogic:
 			[t, h] = grovepi.dht(temphum,0)
@@ -126,9 +151,9 @@ while True:
 #			print "Temperature = {} C, Humidity = {} %".format(temp, hum)
 			api.PostUpdate("{} {} Temp= {} C, Hum= {} %, Dist= {} cm ".format(time.ctime(),msg,temp,hum,dist))		#tweet results
 			doorLogic = door
-			if  door == False:
-				photo = takePhoto()
-				print "ZAP: " + photo
+#			if  door == False:
+#				photo = takePhoto()
+#				print "ZAP: " + photo
 
 #measure temp and hum every app. 1 hour and tweet update or alert
 
@@ -148,8 +173,8 @@ while True:
 #exceptions
 
 	except KeyboardInterrupt as error:
-		resetAll(blue)
-		resetAll(red)
+		for k in led:
+			resetActor(led[k])
 		print "Keyboard Interrupt", str(error)
 		break
 
